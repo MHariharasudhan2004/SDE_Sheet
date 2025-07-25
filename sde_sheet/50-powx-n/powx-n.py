1370763c2f5e568c45b4911623c9b1c59558b062
MHariharasudhan2004 <hariharasudhan.m.2026@gmail.com>
2025-07-25 07:02:38 +0530

    Time: 0 ms (100.00%) | Memory: 17.7 MB (74.56%) - LeetSync

--- sde_sheet/50-powx-n/powx-n.py
+++ sde_sheet/50-powx-n/powx-n.py
@@ -1,17 +1,13 @@
 class Solution:
     def myPow(self, x: float, n: int) -> float:
-        nn=n
-        ans=1.0
-        if nn<0:
-            nn=-1*nn
-        while nn:
-            if nn%2:
-                ans=ans*x
-                nn=nn-1
-            else:
-                x=x*x
-                nn=nn//2
+        if n==0:
+            return 1
         if n<0:
-            ans=1.0/ans
-        return ans
+            x=1/x
+            n=-n
+        half=self.myPow(x,n//2)
+        if n%2:
+            return x*half*half
+        else:
+            return half*half
         
