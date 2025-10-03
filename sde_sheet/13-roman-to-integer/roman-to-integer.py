e58deb5328ad7cca44b570de23403865823b54c3
MHariharasudhan2004 <hariharasudhan.m.2026@gmail.com>
2025-10-03 20:26:57 +0530

    Time: 7 ms (41.43%) | Memory: 17.9 MB (39.87%) - LeetSync

--- sde_sheet/13-roman-to-integer/roman-to-integer.py
+++ sde_sheet/13-roman-to-integer/roman-to-integer.py
@@ -1,11 +1,11 @@
 class Solution:
     def romanToInt(self, s: str) -> int:
-        roman={"I":1,"V":5,"X":10,"L":50,"C":100,"D":500,"M":1000}
+        sym={"I":1,"V":5,"X":10,"L":50,"C":100,"D":500,"M":1000}
         res=0
-        for i in range(len(s)):
-            if i+1<len(s) and roman[s[i]]<roman[s[i+1]]:
-                res-=roman[s[i]]
+        for i in range(len(s)-1):
+            if sym[s[i]]<sym[s[i+1]]:
+                res-=sym[s[i]]
             else:
-                res+=roman[s[i]]
-        return res
+                res+=sym[s[i]]
+        return res+sym[s[-1]]
         
