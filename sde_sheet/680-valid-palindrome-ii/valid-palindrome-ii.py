fe9bac4d5c664cf7bfa6ebb70c3052904659c507
Hariharasudhan M <hariharasudhan.m.2026@gmail.com>
2026-01-10 17:34:14 +0530

    Time: 26 ms (96.18%) | Memory: 19.7 MB (7.76%) - LeetSync

---
+++ sde_sheet/680-valid-palindrome-ii/valid-palindrome-ii.py
@@ -0,0 +1,12 @@
+class Solution:
+    def validPalindrome(self, s: str) -> bool:
+        l=0
+        r=len(s)-1
+        while l<=r:
+            if s[l]!=s[r]:
+                string1=s[:l]+s[l+1:]
+                string2=s[:r]+s[r+1:]
+                return string1[: : -1]==string1 or string2[: : -1]==string2
+            l+=1
+            r-=1
+        return True
