6795d7f35ec7f6c6268834dc38163c6b4d078e5f
Hariharasudhan M <hariharasudhan.m.2026@gmail.com>
2026-01-14 13:44:07 +0530

    Time: 123 ms (77.80%) | Memory: 19.2 MB (30.87%) - LeetSync

--- sde_sheet/647-palindromic-substrings/palindromic-substrings.py
+++ sde_sheet/647-palindromic-substrings/palindromic-substrings.py
@@ -1,25 +1,18 @@
 class Solution:
     def countSubstrings(self, s: str) -> int:
-        resSet=set()
         resLen=0
         for i in range(len(s)):
             l,r=i,i
             while l>=0 and r<len(s) and s[l]==s[r]:
-                index_str=f"{l}:{r}"
-                if index_str not in resSet:
-                    resSet.add(index_str)
-                    resLen+=1
+                resLen+=1
                 l-=1
                 r+=1
             l,r=i,i+1
             while l>=0 and r<len(s) and s[l]==s[r]:
-                index_str=f"{l}:{r}"
-                if index_str not in resSet:
-                    resSet.add(index_str)
-                    resLen+=1
+                resLen+=1
                 l-=1
                 r+=1
-        print(resSet)
+       
         return resLen
             
 
