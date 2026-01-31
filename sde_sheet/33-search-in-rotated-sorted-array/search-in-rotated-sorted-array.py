4406f8edcb562edc2f0808560076117e345377ab
Hariharasudhan M <hariharasudhan.m.2026@gmail.com>
2026-01-31 13:13:23 +0530

    Time: 0 ms (100.00%) | Memory: 19.5 MB (18.14%) - LeetSync

--- sde_sheet/33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
+++ sde_sheet/33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
@@ -1,30 +1,21 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        n=len(nums)
         l=0
-        r=n-1
-        while l<r:
-            m=(l+r)//2
-            if nums[m]>nums[r]:
-                l=m+1
-            else:
-                r=m
-        min_i=l
-        if min_i==0:
-            l,r=0,n-1
-        elif target>=nums[0] and target<=nums[min_i-1]:
-            l,r=0,min_i-1
-        else:
-            l,r=min_i,n-1
-        while l<=r:
-            mid=(l+r)//2
+        r=len(nums)-1
+        while(l<=r):
+            mid=l+(r-l)//2
             if nums[mid]==target:
                 return mid
-            elif nums[mid]>target:
-                r=mid-1
+            #check left half is sorted
+            if nums[l]<=nums[mid]:
+                if nums[l]<=target <nums[mid]:
+                    r=mid-1
+                else:
+                    l=mid+1
             else:
-                l=mid+1
-        return -1
+                if nums[mid]<target<=nums[r]:
+                    l=mid+1
+                else:
+                    r=mid-1
             
-
-        
+        return -1
